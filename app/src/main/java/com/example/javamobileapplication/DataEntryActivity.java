5ea9715fb8e5e78a19c5cad22a065078520cb87e
Indumini Ranasinghe <20205283@student.curtin.edu.au>
2020-09-09 20:07:24 +0530

    Data Entry/Color Values Added, data entry created

---
+++ app/src/main/java/com/example/javamobileapplication/DataEntryActivity.java
@@ -0,0 +1,14 @@
+package com.example.javamobileapplication;
+//Data Entry Activity Backend - 20205283
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class DataEntryActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_data_entry);
+    }
+}
