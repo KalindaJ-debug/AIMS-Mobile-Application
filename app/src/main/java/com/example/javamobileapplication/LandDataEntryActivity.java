73053859446d6efc31b377801aad816857ff1c51
Indumini Ranasinghe <20205283@student.curtin.edu.au>
2020-09-10 09:29:59 +0530

    Data Entry/Data Entry Form part 2 UI completed

---
+++ app/src/main/java/com/example/javamobileapplication/LandDataEntryActivity.java
@@ -0,0 +1,14 @@
+package com.example.javamobileapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class LandDataEntryActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_land_data_entry);
+    }
+}
