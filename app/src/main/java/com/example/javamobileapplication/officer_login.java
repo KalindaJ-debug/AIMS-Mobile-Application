0f9e35d3bcf18d29d772ab3128e42652c36bba22
Rishikeish <20205306@student.curtin.edu.au>
2020-09-10 20:13:47 +0530

    Famer Login and Officer Login

---
+++ app/src/main/java/com/example/javamobileapplication/officer_login.java
@@ -0,0 +1,14 @@
+package com.example.javamobileapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class officer_login extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_officer_login);
+    }
+}
