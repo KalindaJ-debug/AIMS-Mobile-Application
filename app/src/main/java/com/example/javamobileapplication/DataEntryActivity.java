0a8aa02313cf5a7243b2b7848bdb24b1b59f2788
Indumini Ranasinghe <20205283@student.curtin.edu.au>
2020-09-10 09:46:47 +0530

    Data Entry/Data Entry Form Next button implemented

--- app/src/main/java/com/example/javamobileapplication/DataEntryActivity.java
+++ app/src/main/java/com/example/javamobileapplication/DataEntryActivity.java
@@ -2,13 +2,16 @@ package com.example.javamobileapplication;
 //Data Entry Activity Backend - 20205283
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.Spinner;
 
 public class DataEntryActivity extends AppCompatActivity {
 
-
+    private Button btnNext;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,6 +32,20 @@ public class DataEntryActivity extends AppCompatActivity {
         cropCategoryAdapter.setDropDownViewResource(R.layout.support_simple_spinner_dropdown_item);
         cropCategory.setAdapter(cropCategoryAdapter); //set adapter list
 
+        //Button - Next implementation
+        btnNext = (Button) findViewById(R.id.buttonNext);
+        btnNext.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openLandDataEntryActivity();
+            }
+        });
     } //end of onCreate function
+
+    //methods
+    public void openLandDataEntryActivity(){
+        Intent intent = new Intent(this, LandDataEntryActivity.class);
+        startActivity(intent);
+    }//end of open land activity
 
 } //end of DataEntryActivity class
