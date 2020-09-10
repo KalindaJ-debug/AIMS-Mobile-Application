d82c346403508e18b1c2bb8cb730c7d20e78ace9
Indumini Ranasinghe <20205283@student.curtin.edu.au>
2020-09-10 11:22:20 +0530

    Data Entry/Previous button implemented

--- app/src/main/java/com/example/javamobileapplication/LandDataEntryActivity.java
+++ app/src/main/java/com/example/javamobileapplication/LandDataEntryActivity.java
@@ -2,10 +2,18 @@ package com.example.javamobileapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class LandDataEntryActivity extends AppCompatActivity {
 
+    //Declarations
+    //Variables
+    Button btnPrevious; //previous button
+
+    //Database
     DatabaseHelper db;
 
     @Override
@@ -16,6 +24,21 @@ public class LandDataEntryActivity extends AppCompatActivity {
         //instantiate database - SQLite database
         db = new DatabaseHelper(this);
 
+        //Button impl - Previous
+        btnPrevious = (Button) findViewById(R.id.buttonBack);
+        btnPrevious.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openPreviousActivity();
+            }
+        });
+
     } //end of onCreate method
+
+    //Method declaration
+    public void openPreviousActivity(){
+        Intent intent = new Intent(this, DataEntryActivity.class);
+        startActivity(intent);
+    }//end of previous method
 
 } //end of activity class
