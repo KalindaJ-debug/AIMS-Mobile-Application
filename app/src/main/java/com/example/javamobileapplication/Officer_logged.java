89b2fec91f8b8a515adf4de343f6bd62b9a7dc15
Indumini Ranasinghe <20205283@student.curtin.edu.au>
2020-09-30 13:09:54 +0530

    Data Entry/ Integrated Success to Logged In Page

--- app/src/main/java/com/example/javamobileapplication/Officer_logged.java
+++ app/src/main/java/com/example/javamobileapplication/Officer_logged.java
@@ -2,7 +2,9 @@ package com.example.javamobileapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 
 public class Officer_logged extends AppCompatActivity {
@@ -13,6 +15,21 @@ public class Officer_logged extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_officer_logged);
+
+        //declarations
+        btnDataEntry = (Button) findViewById(R.id.button_officer_logged_data_entry);
+        btnDataEntry.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Officer_logged.this.openDataEntryActivity();
+            }
+        });
     }//end of on create method
+
+    //button methods
+    public void openDataEntryActivity(){
+        Intent intent = new Intent(this, DataEntryActivity.class);
+        startActivity(intent);
+    }//end of method
 
 }
