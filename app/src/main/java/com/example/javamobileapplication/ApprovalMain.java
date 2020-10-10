60569e306d8e33063906c93ed0ed733a8eb2b0fe
Indumini Ranasinghe <20205283@student.curtin.edu.au>
2020-10-10 19:52:08 +0530

    Data Entry/commented approval due to firebase service setup inconsistencies

--- app/src/main/java/com/example/javamobileapplication/ApprovalMain.java
+++ app/src/main/java/com/example/javamobileapplication/ApprovalMain.java
@@ -1,151 +1,151 @@
-package com.example.javamobileapplication;
-
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.recyclerview.widget.RecyclerView;
-
-import android.content.ContentValues;
-import android.content.Context;
-import android.content.Intent;
-import android.database.sqlite.SQLiteDatabase;
-import android.os.Bundle;
-import android.provider.BaseColumns;
-import android.util.Log;
-import android.view.View;
-
-import com.google.firebase.FirebaseApp;
-import com.google.firebase.database.DataSnapshot;
-import com.google.firebase.database.DatabaseError;
-import com.google.firebase.database.DatabaseReference;
-import com.google.firebase.database.FirebaseDatabase;
-import com.google.firebase.database.ValueEventListener;
-
-import java.util.List;
-
-public class ApprovalMain extends AppCompatActivity {
-
-    ApprovalDBHelper dbHelper = new ApprovalDBHelper();
-    String TAG = "MyApp";
-    private RecyclerView recyclerView;
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_approval_main);
-
-        dbHelper.setTest("TestValue 1");
-        dbHelper.setTestColumn1("Tests Value 2");
-
-        DatabaseReference reference = FirebaseDatabase.getInstance().getReference();
-        String approvalId = reference.push().getKey();
-        //Approval approval = new Approval("Nelaka", "Jayasinghe", "Western", "Nawala", "Region 1", "Maha", "10/01/2020", (float) 23.567, (float) 35.6, 1, "Test");
-
-        Approval approval = new Approval();
-
-        approval.setFirstName("Nelaka");
-        approval.setLastName("Jayasinghe");
-        approval.setProvince("Western");
-        approval.setDistrict("Nawala");
-        approval.setRegion("Region 1");
-        approval.setSeason("Maha");
-        approval.setSubmitDate("10/01/2020");
-        approval.setCultivatedAmount((float) 23.567);
-        approval.setHarvestAmount((float) 3.567);
-        approval.setStatus(1);
-        approval.setReason("Test");
-
-        reference.child("Approval").push().setValue(approval);
-
-        //reference.child("test").push().setValue(dbHelper);
-        Log.d(TAG, "Test");
-
-        FirebaseDatabase database = FirebaseDatabase.getInstance();
-        DatabaseReference myRef = database.getReference("Approval");
-
-        recyclerView = (RecyclerView) findViewById(R.id.approvalList);
-        new FirebaseDBHelper().readApproval(new FirebaseDBHelper.DataStatus() {
-            @Override
-            public void DataIsLoaded(List<Approval> approvals, List<String> keys) {
-                new RecycleView_Config().setConfig(recyclerView, ApprovalMain.this, approvals, keys);
-            }
-
-            @Override
-            public void DataInserted() {
-
-            }
-
-            @Override
-            public void DataUpdated() {
-
-            }
-
-            @Override
-            public void DataDeleted() {
-
-            }
-        });
-        // Attach a listener to read the data at our posts reference
-//        myRef.addValueEventListener(new ValueEventListener() {
+//package com.example.javamobileapplication;
+//
+//import androidx.appcompat.app.AppCompatActivity;
+//import androidx.recyclerview.widget.RecyclerView;
+//
+//import android.content.ContentValues;
+//import android.content.Context;
+//import android.content.Intent;
+//import android.database.sqlite.SQLiteDatabase;
+//import android.os.Bundle;
+//import android.provider.BaseColumns;
+//import android.util.Log;
+//import android.view.View;
+//
+//import com.google.firebase.FirebaseApp;
+//import com.google.firebase.database.DataSnapshot;
+//import com.google.firebase.database.DatabaseError;
+//import com.google.firebase.database.DatabaseReference;
+//import com.google.firebase.database.FirebaseDatabase;
+//import com.google.firebase.database.ValueEventListener;
+//
+//import java.util.List;
+//
+//public class ApprovalMain extends AppCompatActivity {
+//
+//    ApprovalDBHelper dbHelper = new ApprovalDBHelper();
+//    String TAG = "MyApp";
+//    private RecyclerView recyclerView;
+//
+//    @Override
+//    protected void onCreate(Bundle savedInstanceState) {
+//        super.onCreate(savedInstanceState);
+//        setContentView(R.layout.activity_approval_main);
+//
+//        dbHelper.setTest("TestValue 1");
+//        dbHelper.setTestColumn1("Tests Value 2");
+//
+//        DatabaseReference reference = FirebaseDatabase.getInstance().getReference();
+//        String approvalId = reference.push().getKey();
+//        //Approval approval = new Approval("Nelaka", "Jayasinghe", "Western", "Nawala", "Region 1", "Maha", "10/01/2020", (float) 23.567, (float) 35.6, 1, "Test");
+//
+//        Approval approval = new Approval();
+//
+//        approval.setFirstName("Nelaka");
+//        approval.setLastName("Jayasinghe");
+//        approval.setProvince("Western");
+//        approval.setDistrict("Nawala");
+//        approval.setRegion("Region 1");
+//        approval.setSeason("Maha");
+//        approval.setSubmitDate("10/01/2020");
+//        approval.setCultivatedAmount((float) 23.567);
+//        approval.setHarvestAmount((float) 3.567);
+//        approval.setStatus(1);
+//        approval.setReason("Test");
+//
+//        reference.child("Approval").push().setValue(approval);
+//
+//        //reference.child("test").push().setValue(dbHelper);
+//        Log.d(TAG, "Test");
+//
+//        FirebaseDatabase database = FirebaseDatabase.getInstance();
+//        DatabaseReference myRef = database.getReference("Approval");
+//
+//        recyclerView = (RecyclerView) findViewById(R.id.approvalList);
+//        new FirebaseDBHelper().readApproval(new FirebaseDBHelper.DataStatus() {
 //            @Override
-//            public void onDataChange(DataSnapshot dataSnapshot) {
-//                ApprovalDBHelper post = dataSnapshot.getValue(ApprovalDBHelper.class);
-//                Log.d(TAG, String.valueOf(post));
-//                System.out.println(post);
+//            public void DataIsLoaded(List<Approval> approvals, List<String> keys) {
+//                new RecycleView_Config().setConfig(recyclerView, ApprovalMain.this, approvals, keys);
 //            }
 //
 //            @Override
-//            public void onCancelled(DatabaseError databaseError) {
-//                System.out.println("The read failed: " + databaseError.getCode());
-//            }
-//        });
-//        // Read from the database
-//        myRef.addValueEventListener(new ValueEventListener() {
-//            @Override
-//            public void onDataChange(DataSnapshot dataSnapshot) {
-//                // This method is called once with the initial value and again
-//                // whenever data at this location is updated.
-//                String value = dataSnapshot.getValue(String.class);
-//                Log.d(TAG, "Value is: " + value);
+//            public void DataInserted() {
+//
 //            }
 //
 //            @Override
-//            public void onCancelled(DatabaseError error) {
-//                // Failed to read value
-//                Log.w(TAG, "Failed to read value.", error.toException());
+//            public void DataUpdated() {
+//
+//            }
+//
+//            @Override
+//            public void DataDeleted() {
+//
 //            }
 //        });
-    }
-
-    public void proceedActivity1(View view) {
-        Intent intent = new Intent(this, ApprovalDescription.class);
-        intent.putExtra("farmerFirst", "Nelaka");
-        intent.putExtra("farmerLast", "Jayasinghe");
-        intent.putExtra("district", "Nawala");
-        intent.putExtra("region", "Region 1");
-        intent.putExtra("submittedDate", "06/09/2020");
-        intent.putExtra("harvest", "45.897");
-        startActivity(intent);
-    }
-
-    public void proceedActivity2(View view) {
-        Intent intent = new Intent(this, ApprovalDescription.class);
-        intent.putExtra("farmerFirst", "Kalinda");
-        intent.putExtra("farmerLast", "Jayasinghe");
-        intent.putExtra("district", "Gampaha");
-        intent.putExtra("region", "Region 2");
-        intent.putExtra("submittedDate", "05/12/2020");
-        intent.putExtra("harvest", "78.123");
-        startActivity(intent);
-    }
-
-    public void nextActivity(View view) {
-        Intent intent = new Intent(this, ApprovalDescription.class);
-        intent.putExtra("farmerFirst", "Nelaka");
-        intent.putExtra("farmerLast", "Jayasinghe");
-        intent.putExtra("district", "Nawala");
-        intent.putExtra("region", "Region 1");
-        intent.putExtra("submittedDate", "06/09/2020");
-        intent.putExtra("harvest", "45.897");
-        startActivity(intent);
-    }
-
-}
+//        // Attach a listener to read the data at our posts reference
+////        myRef.addValueEventListener(new ValueEventListener() {
+////            @Override
+////            public void onDataChange(DataSnapshot dataSnapshot) {
+////                ApprovalDBHelper post = dataSnapshot.getValue(ApprovalDBHelper.class);
+////                Log.d(TAG, String.valueOf(post));
+////                System.out.println(post);
+////            }
+////
+////            @Override
+////            public void onCancelled(DatabaseError databaseError) {
+////                System.out.println("The read failed: " + databaseError.getCode());
+////            }
+////        });
+////        // Read from the database
+////        myRef.addValueEventListener(new ValueEventListener() {
+////            @Override
+////            public void onDataChange(DataSnapshot dataSnapshot) {
+////                // This method is called once with the initial value and again
+////                // whenever data at this location is updated.
+////                String value = dataSnapshot.getValue(String.class);
+////                Log.d(TAG, "Value is: " + value);
+////            }
+////
+////            @Override
+////            public void onCancelled(DatabaseError error) {
+////                // Failed to read value
+////                Log.w(TAG, "Failed to read value.", error.toException());
+////            }
+////        });
+//    }
+//
+//    public void proceedActivity1(View view) {
+//        Intent intent = new Intent(this, ApprovalDescription.class);
+//        intent.putExtra("farmerFirst", "Nelaka");
+//        intent.putExtra("farmerLast", "Jayasinghe");
+//        intent.putExtra("district", "Nawala");
+//        intent.putExtra("region", "Region 1");
+//        intent.putExtra("submittedDate", "06/09/2020");
+//        intent.putExtra("harvest", "45.897");
+//        startActivity(intent);
+//    }
+//
+//    public void proceedActivity2(View view) {
+//        Intent intent = new Intent(this, ApprovalDescription.class);
+//        intent.putExtra("farmerFirst", "Kalinda");
+//        intent.putExtra("farmerLast", "Jayasinghe");
+//        intent.putExtra("district", "Gampaha");
+//        intent.putExtra("region", "Region 2");
+//        intent.putExtra("submittedDate", "05/12/2020");
+//        intent.putExtra("harvest", "78.123");
+//        startActivity(intent);
+//    }
+//
+//    public void nextActivity(View view) {
+//        Intent intent = new Intent(this, ApprovalDescription.class);
+//        intent.putExtra("farmerFirst", "Nelaka");
+//        intent.putExtra("farmerLast", "Jayasinghe");
+//        intent.putExtra("district", "Nawala");
+//        intent.putExtra("region", "Region 1");
+//        intent.putExtra("submittedDate", "06/09/2020");
+//        intent.putExtra("harvest", "45.897");
+//        startActivity(intent);
+//    }
+//
+//}
--- app/src/main/java/com/example/javamobileapplication/DataEntryActivity.java
+++ app/src/main/java/com/example/javamobileapplication/DataEntryActivity.java
@@ -2,9 +2,11 @@ package com.example.javamobileapplication;
 //Data Entry Activity Backend - 20205283
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.Spinner;
@@ -15,6 +17,8 @@ import java.util.ArrayList;
 public class DataEntryActivity extends AppCompatActivity {
 
     private Button btnNext;
+    ArrayAdapter<String> crop_list_adapter;
+    ArrayAdapter<String> crop_variety_adapter;
 
     //Database
     DatabaseHelper db;
@@ -26,18 +30,16 @@ public class DataEntryActivity extends AppCompatActivity {
 
         //instantiate database - SQLite database
         db = new DatabaseHelper(this);
-        //seed crop information to tables
-        boolean success = true;
 
         //Declarations - Spinners
-        Spinner cropCategory = (Spinner) findViewById(R.id.spinnerCropCategory);
-        Spinner cropName = (Spinner) findViewById(R.id.spinnerCropName);
+        final Spinner cropCategory = (Spinner) findViewById(R.id.spinnerCropCategory);
+        final Spinner cropName = (Spinner) findViewById(R.id.spinnerCropName);
         final Spinner cropVariety = (Spinner) findViewById(R.id.spinnerCropVariety);
 
         //spinner - crop category implementation
 
         ArrayList<String> crop_category_list = db.getCropCategory();
-        ArrayAdapter<String> crop_category_adapter = new ArrayAdapter<String>(
+        final ArrayAdapter<String> crop_category_adapter = new ArrayAdapter<String>(
                 this,
                 R.layout.support_simple_spinner_dropdown_item,
                 crop_category_list
@@ -46,31 +48,67 @@ public class DataEntryActivity extends AppCompatActivity {
         cropCategory.setAdapter(crop_category_adapter);
         //end crop category spinner
 
-        String selectedCropCategory = cropCategory.getSelectedItem().toString();
-        int selectedCropCategoryID = db.getCropCategoryID(selectedCropCategory);
+        //listen spinner - crop category
 
-        //spinner - crop list implementation
-        ArrayList<String> crop_list = db.getCropList(selectedCropCategoryID);
-        ArrayAdapter<String> crop_list_adapter = new ArrayAdapter<String>(
-                this,
-                R.layout.support_simple_spinner_dropdown_item,
-                crop_list
-        );//end of adapter
-        crop_list_adapter.setDropDownViewResource(R.layout.support_simple_spinner_dropdown_item);
-        cropName.setAdapter(crop_list_adapter);
+        cropCategory.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+
+                ArrayList<String> temp_list = db.getCropList(db.getCropCategoryID(cropCategory.getSelectedItem().toString()));
+                crop_list_adapter = new ArrayAdapter<String>(getApplicationContext(), R.layout.support_simple_spinner_dropdown_item, temp_list);
+                cropName.setAdapter(crop_list_adapter);
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
+
+                String selectedCropCategory = cropCategory.getSelectedItem().toString();
+                int selectedCropCategoryID = db.getCropCategoryID(selectedCropCategory);
+
+                ArrayList<String> crop_list = db.getCropList(selectedCropCategoryID);
+                crop_list_adapter = new ArrayAdapter<String>(
+                        getApplicationContext(),
+                        R.layout.support_simple_spinner_dropdown_item,
+                        crop_list
+                );//end of adapter
+                crop_list_adapter.setDropDownViewResource(R.layout.support_simple_spinner_dropdown_item);
+                cropName.setAdapter(crop_list_adapter);
+
+            }
+        });
+
+        //end of listener spinner
 
         //spinner - crop variety implementation
-        String selectedCrop = cropName.getSelectedItem().toString();
-        int selectedCropID = db.getCropID(selectedCrop);
 
-        ArrayList<String> crop_variety_list = db.getCropVarietyList(selectedCropID);
-        ArrayAdapter<String> crop_variety_adapter = new ArrayAdapter<String>(
-                this,
-                R.layout.support_simple_spinner_dropdown_item,
-                crop_variety_list
-        );//end of adapter
-        crop_variety_adapter.setDropDownViewResource(R.layout.support_simple_spinner_dropdown_item);
-        cropVariety.setAdapter(crop_variety_adapter);
+        //listener for crop name
+        cropName.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+                ArrayList<String> temp_variety_list = db.getCropVarietyList(db.getCropID(cropName.getSelectedItem().toString()));
+                crop_variety_adapter = new ArrayAdapter<String>(getApplicationContext(), R.layout.support_simple_spinner_dropdown_item, temp_variety_list);
+                cropVariety.setAdapter(crop_variety_adapter);
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
+                //default selection
+                String selectedCrop = "Brinjol";
+                int selectedCropID = db.getCropID(selectedCrop);
+
+                ArrayList<String> crop_variety_list = db.getCropVarietyList(selectedCropID);
+                crop_variety_adapter = new ArrayAdapter<String>(
+                        getApplicationContext(),
+                        R.layout.support_simple_spinner_dropdown_item,
+                        crop_variety_list
+                );//end of adapter
+                crop_variety_adapter.setDropDownViewResource(R.layout.support_simple_spinner_dropdown_item);
+                cropVariety.setAdapter(crop_variety_adapter);
+
+            }
+        });
+
+        //end of listener
 
         //Button - Next implementation
         btnNext = (Button) findViewById(R.id.buttonNext);
@@ -81,6 +119,7 @@ public class DataEntryActivity extends AppCompatActivity {
                 openLandDataEntryActivity(variety);
             }
         });
+
     } //end of onCreate function
 
     //methods
