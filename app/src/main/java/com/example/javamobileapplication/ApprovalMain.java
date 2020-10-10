f6d9cc8cfc03e9a579f22244b922aa1b9aeaca29
nel.k <it18067552@my.sliit.lk>
2020-10-10 20:29:48 +0530

    Approval Fix

--- app/src/main/java/com/example/javamobileapplication/ApprovalMain.java
+++ app/src/main/java/com/example/javamobileapplication/ApprovalMain.java
@@ -69,36 +69,6 @@ public class ApprovalMain extends AppCompatActivity {
 
             }
         });
-        // Attach a listener to read the data at our posts reference
-//        myRef.addValueEventListener(new ValueEventListener() {
-//            @Override
-//            public void onDataChange(DataSnapshot dataSnapshot) {
-//                ApprovalDBHelper post = dataSnapshot.getValue(ApprovalDBHelper.class);
-//                Log.d(TAG, String.valueOf(post));
-//                System.out.println(post);
-//            }
-//
-//            @Override
-//            public void onCancelled(DatabaseError databaseError) {
-//                System.out.println("The read failed: " + databaseError.getCode());
-//            }
-//        });
-//        // Read from the database
-//        myRef.addValueEventListener(new ValueEventListener() {
-//            @Override
-//            public void onDataChange(DataSnapshot dataSnapshot) {
-//                // This method is called once with the initial value and again
-//                // whenever data at this location is updated.
-//                String value = dataSnapshot.getValue(String.class);
-//                Log.d(TAG, "Value is: " + value);
-//            }
-//
-//            @Override
-//            public void onCancelled(DatabaseError error) {
-//                // Failed to read value
-//                Log.w(TAG, "Failed to read value.", error.toException());
-//            }
-//        });
     }
 
     public void proceedActivity(View view) {
